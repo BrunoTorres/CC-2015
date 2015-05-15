9a117a0d51a029a1251aff6cbedd0abdf4871b74
João Mano <joaomanooo@gmail.com>
2015-05-15 18:23:10 +0100

    Server

--- MusicGame/src/musicgame/Atendimento.java
+++ MusicGame/src/musicgame/Atendimento.java
@@ -1,4 +1,4 @@
-package musicgame;
+    package musicgame;
 
 import java.io.File;
 import java.io.IOException;
@@ -186,7 +186,7 @@ public class Atendimento extends Thread {
                 listaRanking(data, add, port);
                 break;
             case 14:
-                System.out.println("Procxima pergunta");
+                System.out.println("Proxima pergunta");
                 p = new PDU(data);
                 d = bd.getDesafio(new String(p.getCampo(0).getValor()));
                 int nQuestao = p.getCampo(1).getValor()[0];
@@ -583,44 +583,38 @@ public class Atendimento extends Thread {
             PDU pacote = new PDU(data);
             byte[] tl = {data[2], data[3]};
             int s = PDU.byteArrayToInt(tl);
-            PDU reply;
             Campo c;
             String nome = new String(pacote.getCampo(0).getValor()); //nome
             Desafio d = this.bd.getDesafio(nome);
             int nQ = pacote.getCampo(1).getValor()[1] - 1;               //n questao
             Pergunta p = d.getPergunta(nQ);
-            System.out.println("numero de questao no retransmit: " + nQ);
             int tipo = pacote.getCampo(2).getValor()[0];             //imagem ou audio
             String ca;
             if (tipo == IMAGEM) {
-                System.out.println("Imagem retransmit");
                 ca = this.bd.getPathImage().concat(p.getImagem());
             } else {                
-                System.out.println("Musica retransmit");
                 ca = this.bd.getPathMusic().concat(p.getMusica());
             }
             int bloco = PDU.byteArrayToInt(pacote.getCampo(3).getValor());      //n bloco
-            System.out.println("numero de bloco a transmitir: "+bloco);
 
             TreeMap<Integer, byte[]> blocos = (TreeMap) this.getBlocos(ca);
             byte b[] = blocos.get(bloco);
-
+            
             //byte b[] = this.bd.partes.get(bloco);
             PDU music = new PDU(s, (byte) 0);
             c = new Campo(DESAFIO, nome.getBytes());
-            System.out.println("nome do desafio " + nome);
             music.addCampo(c);                                      //nome
-            c = new Campo(NQUESTAO, PDU.intToByteArray(nQ));
-            System.out.println("numero da questao " + nQ);
+            c = new Campo(NQUESTAO, new byte[]{(byte) nQ});
             music.addCampo(c);                                      //nQuestao
-            c = new Campo(BLOCO, PDU.intToByteArray(bloco));
+            c = new Campo(BLOCO, new byte[]{(byte) bloco});
             music.addCampo(c);                                          //nBloco
-            System.out.println("blocooo " + bloco);
             c = new Campo(tipo, b);
             music.addCampo(c);                                      //bloco
-            System.out.println("Respondeu");
+            if(bloco == blocos.lastKey())
+                music.addCampo(new Campo(FIM, new byte[]{0}));
+            else
+                music.addCampo(new Campo(CONTINUA, new byte[]{0}));
             responde(music, add, port);
-            System.out.println("Depois da resposta");
         } catch (IOException ex) {
             Logger.getLogger(Atendimento.class.getName()).log(Level.SEVERE, null, ex);
         }

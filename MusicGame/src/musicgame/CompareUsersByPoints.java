0e0699d4ce397f800ee5873456b0d1902172bf8b
Patricia <psdr1994@hotmail.com>
2015-05-17 16:26:28 +0100

    no message

--- MusicGame/src/musicgame/CompareUsersByPoints.java
+++ MusicGame/src/musicgame/CompareUsersByPoints.java
@@ -15,15 +15,15 @@ class CompareUsersByPoints implements Comparator<Utilizador>, Serializable {
     @Override
     public int compare(Utilizador o1, Utilizador o2) {
         if (o1.getPontuacao() > o2.getPontuacao()) {
-            return -1;
+            return 1;
         }
         if (o1.getPontuacao() < o2.getPontuacao()) {
-            return 1;
+            return -1;
         }
         if (o1.getTempoResposta() > o2.getTempoResposta()) {
-            return 1;
-        } else {
             return -1;
+        } else {
+            return 1;
         }
     }
 
